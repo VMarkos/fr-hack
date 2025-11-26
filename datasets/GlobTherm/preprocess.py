2c448f0a8cbd335295afe8356227f93b4071f876
bill <vassileiosmarkos@gmail.com>
2025-11-26 11:37:03 +0200

    Create local dataset

--- datasets/GlobTherm/preprocess.py
+++ datasets/GlobTherm/preprocess.py
@@ -19,10 +19,28 @@ class Preprocessor:
             return self._data.describe()
         raise ValueError(f"Data not initialised yet.")
 
+    def filter(self, cname: str, cvals) -> None:
+        clean_vals = { '_'.join(x.split(' ')).lower() for x in cvals }
+        print(clean_vals)
+        self._data = self._data[self._data[cname].isin(clean_vals)]
+
+def _get_csv_dataset(path: str) -> pd.DataFrame:
+    medfish = pd.read_csv(
+        path,
+        sep=',',
+        header=0,
+    )
+    return medfish
 
 if __name__ == "__main__":
     DATAPATH = "data/GlobalTherm_upload_02_11_17.csv"
+    MF_PATH = "../FishBase/medfish.csv"
+    MFTP_PATH = "../MedFaunaTp/MedTemp.csv"
+    MEDFISH = _get_csv_dataset(MF_PATH)
+    MEDFAUNA = _get_csv_dataset(MFTP_PATH)
+    med_species = MEDFAUNA.get('ScientificName')
     pp = Preprocessor(DATAPATH)
+    pp.filter('Species', med_species)
     summary = pp.summary()
     print(summary)
 
---
+++ implementation_log.md
@@ -0,0 +1,7 @@
+# Implementation Log
+
+| Title             | Description                                       |
+| ----------------- | ------------------------------------------------- |
+| Find Med fish     | Scrap all Med sea fish from FishBase              |
+| Get GlobTherm     | Collect thermal tolerance data for saltwater fish |
+| Clean GlobTherm   | Use Medfish dataset to filter GlobTherm data      |
